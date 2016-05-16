4a6d0c19c4e1d47aea24c22eb52270020911e80e
root <antonio.mkp@gmail.com>
2016-05-16 22:20:02 +0200

    aún no recibe

--- recv_test.py
+++ recv_test.py
@@ -12,24 +12,24 @@ import spidev
 
 
 
-pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
-
+pipes =[[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
+server_addres = [0xF0, 0xF0, 0xF0, 0xF0, 0x00];
 radio2 = NRF24(GPIO, spidev.SpiDev())
 radio2.begin(0, 17)
 
 radio2.setRetries(15,15)
 
 radio2.setPayloadSize(32)
-radio2.setChannel(0x60)
-radio2.setDataRate(NRF24.BR_2MBPS)
-radio2.setPALevel(NRF24.PA_MIN)
+radio2.setChannel(76)
+radio2.setDataRate(NRF24.BR_1MBPS)
+radio2.setPALevel(NRF24.PA_MAX)
 
 radio2.setAutoAck(True)
 radio2.enableDynamicPayloads()
 radio2.enableAckPayload()
 
 radio2.openWritingPipe(pipes[0])
-radio2.openReadingPipe(1, pipes[1])
+radio2.openReadingPipe(1, server_addres);
 
 radio2.startListening()
 radio2.stopListening()
@@ -40,7 +40,7 @@ radio2.startListening()
 
 c=1
 while True:
-    akpl_buf = [c,1, 2, 3,4,5,6,7,8,9,0,1, 2, 3,4,5,6,7,8]
+    akpl_buf = [c,2, 2, 3,4,5,6,7,8,9,0,2, 2, 3,4,5,6,7,8]
     pipe = [0]
     while not radio2.available(pipe):
         time.sleep(10000/1000000.0)
@@ -49,10 +49,10 @@ while True:
     radio2.read(recv_buffer, radio2.getDynamicPayloadSize())
     print ("Received:") ,
     print (recv_buffer)
-    c = c + 1
-    if (c&1) == 0:
-        radio2.writeAckPayload(1, akpl_buf, len(akpl_buf))
-        print ("Loaded payload reply:"),
-        print (akpl_buf)
-    else:
-        print ("(No return payload)")
+    # c = c + 1
+    # if (c&1) == 0:
+    #     radio2.writeAckPayload(1, akpl_buf, len(akpl_buf))
+    #     print ("Loaded payload reply:"),
+    #     print (akpl_buf)
+    # else:
+    #     print ("(No return payload)")
